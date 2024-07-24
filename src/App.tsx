057402b08edad0f0dd878974055ac4b79fd3cf09
Jose Espejo <joseespejo@yahoo.com>
2024-07-24 21:43:19 +0100

    feat: add SEAT content

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import "./App.css";
 import Accordion from "./components/Accordion";
+import Tag from "./components/Tag";
 
 function App() {
   return (
     <>
-      <Accordion title="Lorem Ipsum">
-        <h3 className="animated-element fade-in-1 font-bold">Subtitle</h3>
-        <p className="animated-element fade-in-2">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quae
-          culpa veritatis. Corporis fugiat laboriosam modi perspiciatis magni
-          cum est earum harum, ea consequatur laudantium, ab ex enim porro nisi!
+      <Accordion
+        title="SEAT Cars UK"
+        subtitle="Automotive web apps"
+        isExternalOpen={true}
+      >
+        <div className="tags flex gap-2">
+          <Tag text="React" className="animated-element" />
+          <Tag text="TypeScript" className="animated-element" />
+          <Tag text="Hookstate" className="animated-element" />
+          <Tag text="Jahia" className="animated-element" />
+          <Tag text="Cypress" className="animated-element" />
+          <Tag text="Webpack" className="animated-element" />
+        </div>
+        <div className="screenshots my-12 flex min-h-[212px] gap-6">
+          <img
+            src="/seat-finance-calculator-1-192x212.png"
+            alt="SEAT Finance Calculator"
+            className="animated-element overflow-hidden rounded"
+          />
+          <img
+            src="/seat-testdrive-1-258x212.png"
+            alt="SEAT Request a Test Drive"
+            className="animated-element overflow-hidden rounded"
+          />
+          <img
+            src="/seat-testdrive-2-158x212.png"
+            alt="SEAT Request a Test Drive"
+            className="animated-element overflow-hidden rounded"
+          />
+          <img
+            src="/seat-testdrive-3-150x212.png"
+            alt="SEAT Request a Test Drive"
+            className="animated-element overflow-hidden rounded"
+          />
+        </div>
+        {/* <h3 className="animated-element font-bold">Subtitle</h3> */}
+        <p className="animated-element w-6/12 leading-snug text-gray-600">
+          SEAT Finance Calculator and Request a Test Drive forms were built
+          using React and TypeScript and configured using Jahia CMS.
         </p>
-        <div className="animated-element fade-in-3 w-48 h-60 bg-white my-8"></div>
+        <a href="" className="animated-element">
+          www.seat.co.uk
+        </a>
+        <div className="h-8"></div>
       </Accordion>
     </>
   );
