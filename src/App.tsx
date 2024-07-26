fa0353c78360994dc2102693375dfc76a36fec39
Jose Espejo <joseespejo@yahoo.com>
2024-07-26 19:41:29 +0100

    feat: add PortfolioItem
    This commit updates the App component to use the PortfolioItem component instead of the Accordion and Tag components. The Accordion and Tag components are no longer needed and have been removed. This change improves the structure and organization of the code, making it easier to manage and maintain. Additionally, the PortfolioItem component provides better functionality and flexibility for displaying portfolio items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Accordion from "./components/Accordion";
-import Tag from "./components/Tag";
+import PortfolioItem from "./components/PortfolioItem";
+// import PortfolioItem from "./components/PortfolioItem";
 
 function App() {
   const data = {
@@ -29,53 +29,15 @@ function App() {
   const { title, subtitle /* , tags, screenshots, description, link */ } = data;
 
   return (
-    <>
-      <Accordion
-        title="SEAT Cars UK"
-        subtitle="Automotive web apps"
+    <div className="flex flex-col gap-4">
+      <PortfolioItem
+        title={title}
+        subtitle={subtitle}
+        data={data}
         isExternalOpen={true}
-      >
-        <div className="tags flex gap-2">
-          <Tag text="React" className="animated-element" />
-          <Tag text="TypeScript" className="animated-element" />
-          <Tag text="Hookstate" className="animated-element" />
-          <Tag text="Jahia" className="animated-element" />
-          <Tag text="Cypress" className="animated-element" />
-          <Tag text="Webpack" className="animated-element" />
-        </div>
-        <div className="screenshots my-12 flex min-h-[212px] gap-6">
-          <img
-            src="/seat-finance-calculator-1-192x212.png"
-            alt="SEAT Finance Calculator"
-            className="animated-element overflow-hidden rounded"
-          />
-          <img
-            src="/seat-testdrive-1-258x212.png"
-            alt="SEAT Request a Test Drive"
-            className="animated-element overflow-hidden rounded"
-          />
-          <img
-            src="/seat-testdrive-2-158x212.png"
-            alt="SEAT Request a Test Drive"
-            className="animated-element overflow-hidden rounded"
-          />
-          <img
-            src="/seat-testdrive-3-150x212.png"
-            alt="SEAT Request a Test Drive"
-            className="animated-element overflow-hidden rounded"
-          />
-        </div>
-        {/* <h3 className="animated-element font-bold">Subtitle</h3> */}
-        <p className="animated-element w-6/12 leading-snug text-gray-600">
-          SEAT Finance Calculator and Request a Test Drive forms were built
-          using React and TypeScript and configured using Jahia CMS.
-        </p>
-        <a href="" className="animated-element">
-          www.seat.co.uk
-        </a>
-        <div className="h-8"></div>
-      </Accordion>
-    </>
+      />
+      <PortfolioItem title={title} subtitle={subtitle} data={data} />
+    </div>
   );
 }
 
---
+++ src/components/PortfolioItem.tsx
@@ -0,0 +1,164 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import CloseIcon from "./CloseIcon";
+import Tag from "./Tag";
+
+function PortfolioItem({
+  title,
+  subtitle,
+  data,
+  isExternalOpen,
+}: {
+  title: string;
+  subtitle?: string;
+  data: {
+    tags: string[];
+    screenshots: string[];
+    description: string;
+    link: { text: string; url: string };
+  };
+  isExternalOpen?: boolean;
+}) {
+  const mainRef = useRef<HTMLDivElement>(null);
+  const contentContainerRef = useRef<HTMLDivElement>(null);
+  const maxHeightRef = useRef<number>(0);
+  const [isOpen, setIsOpen] = useState(isExternalOpen);
+  const { /* title, subtitle,  */ tags, screenshots, description, link } = data;
+
+  const tl = gsap.timeline();
+
+  useLayoutEffect(() => {
+    if (contentContainerRef?.current) {
+      maxHeightRef.current = contentContainerRef?.current?.offsetHeight;
+    }
+  }, []);
+
+  useEffect(() => {
+    setIsOpen(isExternalOpen);
+  }, [isExternalOpen]);
+
+  useGSAP(
+    () => {
+      gsap.set(".accordion-content", {
+        maxHeight: isOpen ? maxHeightRef.current : "0",
+      });
+
+      gsap.set(".animated-element", { opacity: 0 });
+    },
+    { dependencies: [], scope: mainRef },
+  );
+
+  useGSAP(
+    () => {
+      if (isOpen) {
+        tl.to(`.accordion-content`, {
+          maxHeight: maxHeightRef.current,
+          duration: 0.2,
+        })
+          .to(".subtitle-1", {
+            opacity: 0,
+            duration: 0.2,
+            ease: "power3.out",
+          })
+          .to(
+            ".animated-element",
+            {
+              opacity: 1,
+              duration: 0.2,
+              ease: "power3.in",
+              stagger: 0.1,
+            },
+            "<",
+          );
+      } else {
+        tl.to(".animated-element", {
+          opacity: 0,
+          duration: 0.3,
+          ease: "power3.out",
+        })
+          .to(".subtitle-1", {
+            opacity: 1,
+            duration: 0.3,
+            ease: "power3.in",
+          })
+          .to(
+            `.accordion-content`,
+            {
+              maxHeight: "0",
+              duration: 0.2,
+            },
+            "+=0.1",
+          );
+      }
+    },
+    { dependencies: [isOpen, maxHeightRef.current], scope: mainRef },
+  );
+
+  return (
+    <div
+      ref={mainRef}
+      className={`relative flex flex-col rounded-2xl bg-[hsl(0,0%,85%)] px-8 pb-4 text-black shadow-md shadow-black/30 transition-all duration-1000 ${isOpen ? "pt-8" : "pt-4"} `}
+    >
+      <div
+        className="accordion-header flex cursor-pointer items-baseline gap-2"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <h2 className="text-base font-extrabold text-gray-600">{title}</h2>
+        <h3 className={`subtitle-1 text-xs font-normal text-gray-600`}>
+          {subtitle}
+        </h3>
+        <CloseIcon
+          className={`absolute right-5 top-5 size-5 fill-gray-600 transition-opacity duration-200 ${isOpen ? "opacity-100" : "opacity-0"}`}
+        />
+      </div>
+      <div
+        className={`accordion-content overflow-hidden text-left text-[12px] leading-5`}
+        ref={contentContainerRef}
+      >
+        <div className="subtitle-2 animated-element mt-[-0.25rem] h-14 text-gray-600">
+          {subtitle}
+        </div>
+        <div className="accordion-body">
+          <div className="tags flex gap-2">
+            {tags.map((tag) => (
+              <Tag text={tag} key={tag} className="animated-element" />
+            ))}
+          </div>
+          <div className="screenshots my-12 flex min-h-[212px] gap-6">
+            <img
+              src="/seat-finance-calculator-1-192x212.png"
+              alt="SEAT Finance Calculator"
+              className="animated-element overflow-hidden rounded"
+            />
+            <img
+              src="/seat-testdrive-1-258x212.png"
+              alt="SEAT Request a Test Drive"
+              className="animated-element overflow-hidden rounded"
+            />
+            <img
+              src="/seat-testdrive-2-158x212.png"
+              alt="SEAT Request a Test Drive"
+              className="animated-element overflow-hidden rounded"
+            />
+            <img
+              src="/seat-testdrive-3-150x212.png"
+              alt="SEAT Request a Test Drive"
+              className="animated-element overflow-hidden rounded"
+            />
+          </div>
+          <p className="animated-element w-6/12 leading-snug text-gray-600">
+            {description}
+          </p>
+          <a href={link.url} target="_blank" className="animated-element">
+            {link.text}
+          </a>
+          <div className="h-8"></div>
+        </div>
+        <div className="h-4">{/* spacer */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default PortfolioItem;
