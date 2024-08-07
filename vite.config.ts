43fda9dc2543d33018d66af947317384cc466567
Jose Espejo <joseespejo@yahoo.com>
2024-08-07 10:34:04 +0100

    chore: update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/josef5.github.io/",
+  base: "/josef5.github.io/josef5.github.io/",
   plugins: [react()],
 });
